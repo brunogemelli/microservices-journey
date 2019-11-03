7d5e93db6389d8fd8299919068a2a9c5d0305fcf
bruno <bruno_gemelli@hotmail.com>
2019-11-03 19:42:27 -0300

    Order producer and consumer created
    Rentalapp + Customerapp

--- customer_service/app/Kafka/OrderHandler.php
+++ customer_service/app/Kafka/OrderHandler.php
@@ -2,6 +2,8 @@
 
 namespace App\Kafka;
 
+use App\Models\Order;
+use App\Models\Product;
 use PHPEasykafka\KafkaConsumerHandlerInterface;
 
 class OrderHandler implements KafkaConsumerHandlerInterface
@@ -12,6 +14,37 @@ class OrderHandler implements KafkaConsumerHandlerInterface
         \RdKafka\KafkaConsumer $consumer
     )
     {
-        echo $message->payload;
+        $payload = json_decode($message->payload);
+
+        $order = Order::find($payload->order->id);
+
+        if ($order) {
+            $order->delete();
+        }
+
+        Order::create([
+            'id' => $payload->order->id,
+            'customer_id' => $payload->order->customer_id,
+            'status' => $payload->order->status,
+            'discount' => $payload->order->discount,
+            'total' => $payload->order->total,
+            'date' => $payload->order->date
+        ]);
+
+        foreach ($payload->order->items as $orderItem) {
+            Product::firstOrCreate(
+                ['id' => $orderItem->product->id],
+                ['name' => $orderItem->product->name,]
+            );
+
+            OrderItem::create([
+                'id' => $payload->order->id,
+                'order_id' => $payload->order->order_id,
+                'product_id' => $payload->order->product_id,
+                'qty' => $payload->order->qty,
+                'total' => $payload->order->total,
+            ]);
+        }
+
     }
 }
--- rental_service/app/Observers/OrderObserver.php
+++ rental_service/app/Observers/OrderObserver.php
@@ -6,14 +6,57 @@ use App\Models\Order;
 
 class OrderObserver
 {
+    private $producer;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->producer = new KafkaProducer(
+            $container->get('KafkaBrokerCollection'),
+            'orders',
+            $container->get('KafkaTopicConfig')
+        );
+    }
+
     public function created(Order $order)
     {
         $order->adjustTotal();
+        $this->producer->produce($this->prepareOrder($order)->toJson());
     }
 
     public function updated(Order $order)
     {
         $order->adjustTotal();
         $order->adjustBalance();
+        $this->producer->produce($this->prepareOrder($order)->toJson());
+    }
+
+    public function prepareOrder(Order $order)
+    {
+        $preparedOrder = [
+            'order' => [
+                'id' => $order->id,
+                'customer_id' => $order->customer_id,
+                'status' => $order->status,
+                'discount' => $order->discount,
+                'total' => $order->total,
+                'date' => $order->date,
+            ]
+        ];
+
+        $preparedOrderItems = [];
+
+        foreach ($order->items as $orderItem) {
+            $items['id'] = $orderItem->id;
+            $items['order_id'] = $orderItem->order_id;
+            $items['qty'] = $orderItem->qty;
+            $items['total'] = $orderItem->qty * $orderItem->product->price;
+            $items['product']['id'] = $orderItem->product->id;
+            $items['product']['name'] = $orderItem->product->name;
+            $preparedOrderItems = $items;
+        }
+
+        $preparedOrder['order']['items'] = $preparedOrderItems;
+
+        return $preparedOrder;
     }
 }
