a4173402ed74236bed9f1e8e19cfdfe4938a6e2f
bruno <bruno_gemelli@hotmail.com>
2019-11-03 21:56:26 -0300

    Rentalapp - SSO
    small fixes

--- rental_service/app/Providers/EventServiceProvider.php
+++ rental_service/app/Providers/EventServiceProvider.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
